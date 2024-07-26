cba2dc8044ba6ddcf4003d447ff43b3cb2ac299f
MRajkumar404 <rajroman139.com>
2024-07-26 16:46:24 +0530

    Password Validation

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import re
 
 app = Flask(__name__)
-
-@app.cli.command('create-db')
-def create_db():
-    """Create database tables."""
-    with app.app_context():
-        db.create_all()
-        print("Database tables created.")
-
+app.secret_key = 'supersecretkey'  # Needed for flash messages
 
 # Set the database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///loginform.db'
@@ -18,29 +12,54 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Initialize the database with the app
 db = SQLAlchemy(app)
 
-@app.route('/',methods=['GET'])
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
 
+@app.route('/')
 def index():
-    
-    # Pass the data to the HTML template
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users, show_signup=True)
 
-@app.route('/',methods=['POST'])
+@app.route('/add', methods=['POST'])
 def add():
     user_name = request.form['usr_na']
-    print(user_name)
     email = request.form['usr_em']
-    print(email)
-    passwo = request.form['usr_pas']
-    print(passwo)
-    con_pass = request.form['usr_cnfr']
-    print(con_pass)
+    password = request.form['usr_pas']
+    confirm_password = request.form['usr_cnfr']
+
+    # Regular expression for password validation
+    password_regex = re.compile(r'^(?=.*\d)(?=.*[!@#$%^&*])[0-9a-zA-Z!@#$%^&*]{6,}$')
+
+    if not password_regex.match(password):
+        flash('Password must be at least 6 characters long and include at least one number and one special character.', 'error')
+        users = User.query.all()
+        return render_template('index.html', user_name=user_name, email=email, users=users, show_signup=True)
+
+    if password != confirm_password:
+        flash('Passwords do not match. Please try again.', 'error')
+        users = User.query.all()
+        return render_template('index.html', user_name=user_name, email=email, users=users, show_signup=True)
+
+    new_user = User(username=user_name, email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    flash('User added successfully!', 'success')
     return redirect('/')
 
+@app.route('/delete_user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify(success=True)
+    return jsonify(success=False)
 
 if __name__ == '__main__':
-    # Create database tables
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
